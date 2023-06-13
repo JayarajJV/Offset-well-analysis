16383b3229782dbab53fe2b243470490f34da6b9
JayarajJV <136434386+JayarajJV@users.noreply.github.com>
2023-06-13 17:23:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,15 @@ def add_bg_from_url():
 
 add_bg_from_url() 
     
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
 
 #add_bg_from_local(img_path)    
 st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
